f371a38119e27e41ed3cadb0df24cbd13f723e5c
Kim Gimun <kmkim7575@gmail.com>
2023-12-08 23:51:37 +0900

    add footer for information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,62 @@
 import React from 'react'
-import { FaDribbbleSquare, FaFacebookSquare, FaGithubSquare, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { FaDribbbleSquare, FaFacebookSquare, FaGithub, FaGithubSquare, FaInstagram, FaTwitter, FaTwitterSquare } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='m-w-[1520px] m-auto px-4 py-2 bg-[#24262b]'>
+        <div className='py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+            <div>
+                <h1 className='w-full text-3xl font-bold text-orange-500'>YumEats</h1>
+                <p>eateateateaetjladkjlajsfdlkj aljkwekfsafjkdlkj djfakljfwk fdjsaljkwel dsjalkfjlwe</p>
+                <div className='flex justify-between md:w-[75%] my-6'>
+                    <FaFacebookSquare size={30} />
+                    <FaInstagram size={30} />
+                    <FaTwitterSquare size={30} />
+                    <FaGithubSquare size={30} />
+                    <FaDribbbleSquare size={30} />
+                </div>
+
+            </div>
+            <div className='lg:col-span-2 flex justify-between mt-6'>
+                <div>
+                    <h6 className='fonts-medium text-[#9b9b9b]'>Location</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>New York</li>
+                        <li className='py-2 text-sm'>USA</li>
+                        <li className='py-2 text-sm'>India</li>
+                        <li className='py-2 text-sm'>Canada</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='fonts-medium text-[#9b9b9b]'>Location</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>New York</li>
+                        <li className='py-2 text-sm'>USA</li>
+                        <li className='py-2 text-sm'>India</li>
+                        <li className='py-2 text-sm'>Canada</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='fonts-medium text-[#9b9b9b]'>Location</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>New York</li>
+                        <li className='py-2 text-sm'>USA</li>
+                        <li className='py-2 text-sm'>India</li>
+                        <li className='py-2 text-sm'>Canada</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='fonts-medium text-[#9b9b9b]'>Location</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>New York</li>
+                        <li className='py-2 text-sm'>USA</li>
+                        <li className='py-2 text-sm'>India</li>
+                        <li className='py-2 text-sm'>Canada</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
